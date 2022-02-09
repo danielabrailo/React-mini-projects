7bde3001ebaa86dff2529e6ebdd6c3d984a188d6
Daniela Brailovsky <dbrailo@gmail.com>
2022-02-09 19:38:01 +0200

    added remove option

--- todo-app-react-ts/src/components/NewTodo.tsx
+++ todo-app-react-ts/src/components/NewTodo.tsx
@@ -1,6 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useContext } from "react";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+import { TodosContext } from "../store/todos-context";
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC = () => {
+  const todoCtx = useContext(TodosContext);
+
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -13,11 +18,11 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todoCtx.addTodo(enteredText);
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
